be617f1714ac79017f26ed8aa0f2ff130014b844
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-07 10:54:07 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+// random
+let random = Math.floor(Math.random() * 8);
+
+// modules
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+//max
+let max = Math.max(10,15,20);
